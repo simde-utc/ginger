13e5d15e603c90b4fb8a854457f1169bd533a07a
Arthur Puyou <arthur@puyou.fr>
2012-10-06 22:45:44 +0100

    Déplacement de la classe api et rangement de l'index

--- web/index.php
+++ web/index.php
@@ -1,20 +1,19 @@
 <?php
-require '../lib/Slim/Slim.php';
-require '../lib/api.php';
-
 // Include the main Propel script, initialize it and add classes to inc path
 require_once '../lib/propel/runtime/lib/Propel.php';
 Propel::init("../build/conf/ginger-conf.php");
 set_include_path("../build/classes" . PATH_SEPARATOR . get_include_path());
 
+require_once '../lib/Slim/Slim.php';
 \Slim\Slim::registerAutoloader();
-
 $app = new \Slim\Slim();
 $app->contentType('application/json; charset=utf-8');
+
+
+require_once '../class/Api.class.php';
 $api = new Api($app);
 
 require_once '../class/ginger.class.php';
-
 
 $app->get('/v1/:login', function ($login) use ($api) {
 	$ginger = new Ginger($_GET['key']);
