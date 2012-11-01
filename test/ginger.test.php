4ed22894fcd1a9b60a7e9f26ed5b01cdf013b2ee
Arthur Puyou <arthur@puyou.fr>
2012-11-01 20:30:05 +0000

    Send no password when password config is empty

--- test/ginger.test.php
+++ test/ginger.test.php
@@ -52,7 +52,13 @@ class GingerTest extends PHPUnit_Extensions_Database_TestCase
 	 * get db connection
 	 */
 	public function getConnection() {
-		$pdo = new PDO('mysql:dbname='.$GLOBALS['SQL_DB'].';host='.$GLOBALS['SQL_HOST'], $GLOBALS['SQL_USER'], $GLOBALS['SQL_PASSWORD']);
+		if(empty($GLOBALS['SQL_PASSWORD'])){
+			$pdo = new PDO('mysql:dbname='.$GLOBALS['SQL_DB'].';host='.$GLOBALS['SQL_HOST'], $GLOBALS['SQL_USER']);
+		}
+		else {
+			$pdo = new PDO('mysql:dbname='.$GLOBALS['SQL_DB'].';host='.$GLOBALS['SQL_HOST'], $GLOBALS['SQL_USER'], $GLOBALS['SQL_PASSWORD']);
+		}
+		
 		return $this->createDefaultDBConnection($pdo);
 	}
 
